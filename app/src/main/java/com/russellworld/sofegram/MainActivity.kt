5373c7042bb7b9930f2198ed586e3a67b4a2732f
RassellWorld <miraqq55@gmail.com>
2021-06-29 10:10:22 +0200

    Создание бокового меню Material Drawer

--- app/src/main/java/com/russellworld/sofegram/MainActivity.kt
+++ app/src/main/java/com/russellworld/sofegram/MainActivity.kt
@@ -1,15 +1,69 @@
 package com.russellworld.sofegram
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.mikepenz.materialdrawer.AccountHeader
+import com.mikepenz.materialdrawer.AccountHeaderBuilder
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem
 import com.russellworld.sofegram.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var mBinding: ActivityMainBinding
+    lateinit var mDrawer: Drawer
+    lateinit var mHeader: AccountHeader
+    lateinit var mToolBar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
     }
+
+    override fun onStart() {
+        super.onStart()
+        initFields()
+        initFun()
+    }
+
+    private fun initFields() {
+        mToolBar = mBinding.mainToolbar
+    }
+
+    private fun initFun() {
+        setSupportActionBar(mToolBar)
+        createHeader()
+        createDrawer()
+
+    }
+
+    private fun createDrawer() {
+        mDrawer = DrawerBuilder()
+            .withActivity(this)
+            .withToolbar(mToolBar)
+            .withActionBarDrawerToggle(true)
+            .withSelectedItem(-1)
+            .withAccountHeader(mHeader)
+            .addDrawerItems(
+                ProfileDrawerItem().withIdentifier(100)
+                    .withIcon(R.drawable.material_drawer_circle_mask)
+                    .withName("Создать группу")
+                    .withSelectable(false)
+            ).build()
+    }
+
+    private fun createHeader() {
+        mHeader = AccountHeaderBuilder()
+            .withActivity(this)
+            .withHeaderBackground(R.drawable.header)
+            .addProfiles(
+                ProfileDrawerItem().withName("Ruslan Mir")
+                    .withEmail("+37592882882")
+            ).build()
+    }
+
+
 }
