d483f22b1e71f51a2da937ffc5afff8e5855e546
RassellWorld <miraqq55@gmail.com>
2021-07-01 19:07:57 +0200

    Добавление Options menu с иконками

--- app/src/main/java/com/russellworld/sofegram/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/russellworld/sofegram/ui/fragments/SettingsFragment.kt
@@ -1,8 +1,19 @@
 package com.russellworld.sofegram.ui.fragments
 
+import android.view.Menu
+import android.view.MenuInflater
 import com.russellworld.sofegram.R
 
 
 class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
+
+    override fun onResume() {
+        super.onResume()
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        activity?.menuInflater?.inflate(R.menu.settings_action_menu, menu)
+    }
 
 }
