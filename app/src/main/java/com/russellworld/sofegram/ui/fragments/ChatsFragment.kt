1c405ac1630e38faff28641857f1de4e980570c2
RassellWorld <miraqq55@gmail.com>
2021-09-04 14:23:11 +0300

    Отправка сообщений в Firebase

--- app/src/main/java/com/russellworld/sofegram/ui/fragments/ChatsFragment.kt
+++ app/src/main/java/com/russellworld/sofegram/ui/fragments/ChatsFragment.kt
@@ -2,6 +2,7 @@ package com.russellworld.sofegram.ui.fragments
 
 import androidx.fragment.app.Fragment
 import com.russellworld.sofegram.R
+import com.russellworld.sofegram.utilits.APP_ACTIVITY
 
 /* Главный фрагмент, содержит все чаты, группы и каналы с которыми взаимодействует пользователь*/
 
@@ -9,6 +10,7 @@ class ChatsFragment : Fragment(R.layout.fragment_chats) {
 
     override fun onResume() {
         super.onResume()
+        APP_ACTIVITY.title = "Чаты"
 
     }
 }
--- app/src/main/java/com/russellworld/sofegram/ui/fragments/SingleChatFragment.kt
+++ app/src/main/java/com/russellworld/sofegram/ui/fragments/SingleChatFragment.kt
@@ -7,6 +7,7 @@ import com.russellworld.sofegram.models.CommonModel
 import com.russellworld.sofegram.models.UserModel
 import com.russellworld.sofegram.utilits.*
 import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.fragment_single_chat.*
 import kotlinx.android.synthetic.main.toolbar_info.view.*
 
 
@@ -28,7 +29,16 @@ class SingleChatFragment(private val contact: CommonModel) : BaseFragment(R.layo
 
         mRefUser = REF_DATABASE_ROOT.child(NODE_USERS).child(contact.id)
         mRefUser.addValueEventListener(mListenerInfoToolbar)
+        chat_btn_send_message.setOnClickListener {
+            val message = chat_input_message.text.toString()
+            if (message.isEmpty()) {
+                showToast("Введите сообщение")
+            }else sendMessage(message, contact.id, TYPE_TEXT){
+                chat_input_message.setText("")
+            }
+        }
     }
+
 
     private fun initInfoToolbar() {
         if (mReceivingUser.fullname.isEmpty()) {
