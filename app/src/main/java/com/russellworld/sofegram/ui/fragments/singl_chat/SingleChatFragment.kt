0b0023c86264177b72e06f114f5c216d7e1e877c
RassellWorld <miraqq55@gmail.com>
2021-09-17 11:51:30 +0300

    Отправка картинок в чат

--- app/src/main/java/com/russellworld/sofegram/ui/fragments/singl_chat/SingleChatFragment.kt
+++ app/src/main/java/com/russellworld/sofegram/ui/fragments/singl_chat/SingleChatFragment.kt
@@ -1,5 +1,7 @@
 package com.russellworld.sofegram.ui.fragments.singl_chat
 
+import android.app.Activity
+import android.content.Intent
 import android.view.View
 import android.widget.AbsListView
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -11,7 +13,10 @@ import com.russellworld.sofegram.models.CommonModel
 import com.russellworld.sofegram.models.UserModel
 import com.russellworld.sofegram.ui.fragments.BaseFragment
 import com.russellworld.sofegram.utilits.*
+import com.theartofdev.edmodo.cropper.CropImage
+import com.theartofdev.edmodo.cropper.CropImageView
 import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.fragment_settings.*
 import kotlinx.android.synthetic.main.fragment_single_chat.*
 import kotlinx.android.synthetic.main.toolbar_info.view.*
 
@@ -43,6 +48,24 @@ class SingleChatFragment(private val contact: CommonModel) : BaseFragment(R.layo
     private fun initFields() {
         mLayoutManager = LinearLayoutManager(this.context)
         mSwipeRefreshLayout = chat_swipe_refresh
+        chat_input_message.addTextChangedListener(AppTextWatcher {
+            val string = chat_input_message.text.toString()
+            if (string.isEmpty()) {
+                chat_btn_send_message.visibility = View.GONE
+                chat_btn_attach.visibility = View.VISIBLE
+            } else {
+                chat_input_message.visibility = View.VISIBLE
+                chat_btn_attach.visibility = View.GONE
+            }
+        })
+        chat_btn_attach.setOnClickListener { attachFile() }
+    }
+
+    private fun attachFile() {
+        CropImage.activity()
+            .setAspectRatio(1, 1)
+            .setRequestedSize(600, 600)
+            .start(APP_ACTIVITY, this)
     }
 
     private fun initRecyclerView() {
@@ -133,6 +156,31 @@ class SingleChatFragment(private val contact: CommonModel) : BaseFragment(R.layo
         mToolbarInfo.toolbar_chat_image.downloadAndSetImage(mReceivingUser.photoUrl)
         mToolbarInfo.toolbar_chat_status.text = mReceivingUser.state
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        /* Активность которая запускается для получения картинки для фото пользователя */
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE
+            && resultCode == Activity.RESULT_OK && data != null
+        ) {
+            val uri = CropImage.getActivityResult(data).uri
+            val messageKey = REF_DATABASE_ROOT
+                .child(NODE_MESSAGES)
+                .child(CURRENT_UID)
+                .child(contact.id).push().key.toString()
+
+            val path = REF_STORAGE_ROOT
+                .child(FOLDER_MESSAGE_IMAGE)
+                .child(messageKey)
+
+            putImageToStorage(uri, path) {
+                getUrlFromStorage(path) {
+sendMessageAsImage(contact.id, it, messageKey)
+                }
+            }
+        }
+    }
+
 
     override fun onPause() {
         super.onPause()
